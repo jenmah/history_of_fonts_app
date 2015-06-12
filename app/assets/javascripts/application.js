4e1ee9fcff5bf975ed9dc97b699cf950255116e0
Jennifer Mah <jen.mah@hotmail.com>
2015-06-12 08:47:09 +0100

    styled buttons on search page edit and delete functions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,6 @@
 //= require_tree .
 
 
-
 $(document).ready(function() {
 
   function addSuggestion() {
@@ -25,9 +24,7 @@ $(document).ready(function() {
     var suggestionReason = $('#suggestion-reason').val();
     console.log(suggestedFont);
     console.log(suggestionReason);
-
     $('#suggestions-list').prepend('<p>' + "Thanks! Your suggestion has been added to our database." + '</p>');
-
     $.ajax({
       type: 'POST',
       url: '/suggestions',
@@ -38,9 +35,7 @@ $(document).ready(function() {
       }
     }).done(function(suggestion) {
       var id = suggestion["id"];
-
       $('#suggestions-list').prepend('<h4>' + suggestion['suggested_font'] + '</h4>' + '<button class="edit-suggestion-box" data-id="' + id + '">' + 'EDIT' + '</button>' + '<button class="delete-suggestion-box" data-id="' + id + '">' + 'DELETE' + '</button>');
-
       console.log(suggestion.suggested_font);
       $('#suggested-font').val('');
       $('#suggestion-reason').val('');
@@ -52,7 +47,6 @@ $(document).ready(function() {
     var deleteBox = $(this);
     var deleteBoxId = $(this).data('id');
     console.log(deleteBoxId);
-
     $.ajax({
       type: 'DELETE',
       url: "/suggestions/" + deleteBoxId
@@ -63,6 +57,7 @@ $(document).ready(function() {
   }
 
 
+
   function editSuggestion() {
     console.log('editSuggestion');
     var editBoxId = $(this).data('id');
@@ -70,7 +65,7 @@ $(document).ready(function() {
     $('#suggested-font').val("testing");
     $('#suggestion-reason').val("testing");
 
-    $('#suggestions-list').html('<h4>' + 'Edit your suggestion above' + '</h4>');
+    $('#suggestions-list').html('<p>' + 'Edit your suggestion above' + '</p>');
 
     $.ajax({
       type: 'GET',
@@ -82,6 +77,8 @@ $(document).ready(function() {
     })
   }
 
+
+  // Click Events
   $('#submit-suggestion').on('click', addSuggestion);
 
   $('body').delegate('.delete-suggestion-box', 'click', deleteSuggestion);
